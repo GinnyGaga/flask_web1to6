e45dc055359673064492019051dc51a6f0865294
mo <mo@example.com>
2017-08-17 22:14:38 +0800

    add _int

--- hello.py
+++ hello.py
@@ -9,6 +9,10 @@ def index():
 def my_name(name):
 	return 'hello,<i>%s</i>' % name
 
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+	return 'my post <strong>%d</strong>' % post_id
+
 if __name__=='__main__':
 	app.run(debug=True)
 
