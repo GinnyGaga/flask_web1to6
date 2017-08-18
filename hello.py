9200d96cca05964dc7addf2b0c565683721bef03
mo <mo@example.com>
2017-08-18 21:01:07 +0800

    404

--- hello.py
+++ hello.py
@@ -1,17 +1,15 @@
 from flask import Flask
+from flask import abort
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return '<h1>Hello World</h1>'
 
-@app.route('/user/<name>')
-def my_name(name):
-	return 'hello,<i>%s</i>' % name
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-	return 'my post <strong>%d</strong>' % post_id
+@app.route('/user/<id>')
+def get_user(id):
+	user=load_user(id)
+	if not user:
+		abort(404)
+			
+	return '<h1>hello,%s</h1>' % user.name
 
 if __name__=='__main__':
 	app.run(debug=True)
