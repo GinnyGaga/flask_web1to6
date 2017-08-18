cdff20e8f541c61b654a670d6f216c822404e622
mo <mo@example.com>
2017-08-18 20:52:38 +0800

    400

--- hello.py
+++ hello.py
@@ -3,11 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World</h1>'
+	return '<h1>bad request</h1>',400
 
-@app.route('/user/<name>')
-def my_name(name):
-	return 'hello,<i>%s</i>' % name
 
 if __name__=='__main__':
 	app.run(debug=True)
