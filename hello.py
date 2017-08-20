e8933c366610c81908c2571027bb9b538802195e
mo <mo@example.com>
2017-08-21 07:37:36 +0800

    404/500

--- hello.py
+++ hello.py
@@ -18,7 +18,8 @@ def internal_server_error(e):
 @app.route('/')
 def index():
 	return render_template('index.html')
-@app.route('/user/name')
+
+@app.route('/user/<name>')
 def user(name):
 	return render_template('user.html',name=name)
 
