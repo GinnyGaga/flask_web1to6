9c8b053ae9dc37ce2b191111e01f9c9f9c336390
mo <mo@example.com>
2017-08-21 22:16:28 +0800

    Flask-Moment

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 
 manager=Manager(app)
 bootstrap=Bootstrap(app)
+moment=Moment(app)
+
+@app.route('/')
+def index():
+	return render_template('index.html',
+							current_time=datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,10 +22,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'),500
-
-@app.route('/')
-def index():
-	return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
